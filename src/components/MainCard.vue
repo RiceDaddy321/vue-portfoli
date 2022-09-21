7c7427aab7d4b572b2ceed1229f01c95bd8378e1
RiceDaddy321 <0carlitos2@gmail.com>
2022-09-21 11:48:13 -0500

    Initial Commit

---
+++ src/components/MainCard.vue
@@ -0,0 +1,155 @@
+<template>
+  <div class="wrapper">
+  <div class="main-container">
+    <div class="container-one">
+      <!--Put images here along with job titles and other experience, and links-->
+      <div class="profile">
+        <img src="../assets/profilepic.jpg" alt="" />
+        <h3>Name here</h3>
+        <p>Software Developer</p>
+      </div>
+      <div class="social-links">
+        <div class="link">
+          <a href="https://github.com/RiceDaddy321" target="_blank" class="">
+            <img src="../assets/GitHub-Mark/PNG/GitHub-Mark-64px.png" alt="" />
+            <h2>GitHub</h2>
+          </a>
+        </div>
+        <div class="link">
+          <a href="https://www.linkedin.com/in/jc-velazquez/" target="_blank" class="">
+            <img src="../assets/LinkedIn-Logos/LI-In-Bug.png" alt="" />
+            <h2>LinkedIn</h2>
+          </a>
+        </div>
+        <div class="link">
+          <a href="https://utrgv.joinhandshake.com/stu/users/31851884" target="_blank" class="">
+            <img src="../assets/handshakeLogo.webp" alt="" />
+            <h2>Handshake</h2>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="container-two">
+      <div class="about-me">
+        <h1>About Me</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci, beatae
+          quis velit quo fugiat molestiae ipsa porro illum, labore quam ut soluta
+          laudantium sint suscipit dolore dolores rem. Accusamus.
+        </p>
+      </div>
+      <div class="technologies">
+        <h1>Technologies Studied</h1>
+        <div class="logos">
+          <!-- TODO: put all of the technology logos here, or create a cool bar to represent profieciency -->
+          <img src="../assets/C++Logo.png" alt="" />
+          <img src="../assets/pythonLogo.png" alt="">
+          <img src="../assets/JavaScript-logo.png" alt="">
+          <img src="../assets/logo.png" alt="">
+        </div>
+      </div>
+      <!-- temporary, not sure where to put -->
+      <div class="button-container">
+        <div class="projects-button">
+          <a href="">My Projects</a>
+        </div>
+        <div class="projects-button">
+          <a :href="`${publicPath}Resume.pdf`" target="_blank">My Resume</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+      return {
+        publicPath: process.env.BASE_URL
+      }
+    }
+};
+</script>
+
+<style scoped>
+img {
+  /* width: 30px; */
+}
+.wrapper {
+  display: flex;
+  justify-content: center;
+}
+
+.main-container {
+  display: flex;
+  flex-direction: row;
+  width: 60%;
+  background-color: rgb(155, 141, 141);
+  border-radius: 25px;
+}
+
+.container-one {
+  background-color: pink;
+  padding: 2.5%;
+  width: 40%;
+  border-radius: 25px;
+
+}
+
+.link a {
+  text-decoration: none;
+  color: black;
+  font-size: 10px;
+}
+
+.link img {
+    height:2rem;
+    padding: 0.2rem;
+    border-color: rgb(188, 105, 158);
+    border-width: thin;
+    box-shadow: rgb(190, 118, 118) 2px 2px 2px;
+    border-style: solid;
+}
+
+.logos {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.logos img {
+  height: 2.5rem;
+  margin: 2%;
+}
+
+.profile img {
+  height: 5rem;
+  border-radius: 50%;
+}
+
+.container-two {
+  background-color: lightgreen;
+  margin: 2.5%;
+  width: 60%;
+}
+
+.button-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-content: center;
+}
+
+.projects-button {
+  background-color: purple;
+  margin: 5px;
+  padding: 0.5rem;
+  border-radius: 0.8rem;
+}
+
+.projects-button a{
+  text-decoration: none;
+  color: whitesmoke;
+}
+</style>
---
+++ src/components/ProjectsCard.vue
@@ -0,0 +1,45 @@
+<template>
+  <!-- {{ projectData }} -->
+  <div class="main-container">
+    <div
+      class="project-card"
+      v-for="(items, index) in projectData.projectsArray"
+      v-bind:key="items"
+    >
+      <h2>{{ projectData.projectsArray[index].title }}</h2>
+      <img v-bind:src="projectData.projectsArray[index].image" alt="" />
+      <p>{{ projectData.projectsArray[index].about }}</p>
+    </div>
+  </div>
+</template>
+<script>
+import jsonData from "../../projects.json";
+export default {
+  data() {
+    return {
+      projectData: jsonData,
+    };
+  },
+};
+</script>
+<style scoped>
+.project-card img {
+  height: 2rem;
+}
+/* This is how to display elements two per row but vertical on mobile */
+.main-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+}
+
+.project-card {
+    margin: 10px;
+    padding: 10px;
+    flex-wrap: 0 0 40%;
+    background-color:white;
+    border-radius: 25px;
+    box-shadow: rgb(58, 54, 54) 0px 3px 8px;
+}
+</style>
